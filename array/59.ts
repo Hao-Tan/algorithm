01962f4e410d6af6f8e9b23d288158d046ec7876
Frank Tan <909628250@qq.com>
2021-04-13 02:10:48 +0800

    59.ts 螺旋矩阵

--- array/59.ts
+++ array/59.ts
@@ -6,31 +6,31 @@ function generateMatrix(n: number): number[][] {
     const arr = new Array(n).fill(0).map((item) => new Array(n).fill(0));
     let startX = 0;
     let startY = 0;
+    let endX = n - 1;
+    let endY = n - 1;
     let loop = n >> 1;
     let mid = n >> 1;
-    let length = n - 1;
     let num = 1;
 
     while (loop--) {
-        let i = startX;
-        let j = startY;
         // 从左到右，左闭右开，每次循环圈大小减1的次数
-        for (; j < startY + length; j++) {
-            arr[i][j] = num++;
+        for (let j = startY; j < endY; j++) {
+            arr[startX][j] = num++;
         }
-        for (; i < startX + length; i++) {
-            arr[i][j] = num++;
+        for (let i = startX; i < endX; i++) {
+            arr[i][endY] = num++;
         }
-        for (; j > startX; j--) {
-            arr[i][j] = num++;
+        for (let j = endY; j > startY; j--) {
+            arr[endX][j] = num++;
         }
-        for (; i > startY; i--) {
-            arr[i][j] = num++;
+        for (let i = endX; i > startX; i--) {
+            arr[i][startX] = num++;
         }
 
-        length -= 2;
         startX++;
         startY++;
+        endX--;
+        endY--;
     }
 
     if (n % 2 === 1) {
